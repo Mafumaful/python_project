b1bc9c6531954ec35a9ad209ccb545155298ff33
mafumaful <s6bian@uwaterloo.ca>
2023-03-24 23:15:40 -0400

    test json file

---
+++ json_test/test1.py
@@ -0,0 +1,16 @@
+# read the json file
+import json
+
+with open('test.json') as f:
+    data = json.load(f)
+
+# print the data
+print(data)
+# print the type of data
+print(type(data))
+# print the data of the key 'name'
+print(data['name'])
+# print the address of the key 'address'
+print(data['address'])
+# print the address of the key 'address' and the key 'street'
+print(data['address']['street'])
