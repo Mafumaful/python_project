74920f7ba521ae988463dfe7d01bf7f19fa1cfbb
mafumaful <s6bian@uwaterloo.ca>
2023-09-07 17:31:32 -0400

    default

---
+++ gym_learning/DonkeyKong.py
@@ -0,0 +1,11 @@
+import gymnasium as gym
+env = gym.make("ALE/DonkeyKong-v5", render_mode="human")
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+   action = env.action_space.sample()  # this is where you would insert your policy
+   observation, reward, terminated, truncated, info = env.step(action)
+
+   if terminated or truncated:
+      observation, info = env.reset()
+
+env.close()
---
+++ gym_learning/LunarLander/train.py
@@ -0,0 +1,65 @@
+from test import test
+from model import ActorCritic
+import torch
+import torch.optim as optim
+import gymnasium as gym
+
+def train():
+    # Defaults parameters:
+    #    gamma = 0.99
+    #    lr = 0.02
+    #    betas = (0.9, 0.999)
+    #    random_seed = 543
+
+    render = False
+    gamma = 0.99
+    lr = 0.02
+    betas = (0.9, 0.999)
+    random_seed = 543
+    
+    torch.manual_seed(random_seed)
+    
+    env = gym.make('LunarLander-v2')
+    env.reset(seed=random_seed)
+    
+    policy = ActorCritic()
+    optimizer = optim.Adam(policy.parameters(), lr=lr, betas=betas)
+    print(lr,betas)
+    
+    running_reward = 0
+    for i_episode in range(0, 10000):
+        state,info = env.reset()
+        for t in range(10000):
+            action = policy(state)
+            state, reward, terminated, truncated, info = env.step(action)
+            policy.rewards.append(reward)
+            running_reward += reward
+            if render and i_episode > 1000:
+                env.render()
+            if terminated or truncated:
+                break
+                    
+        # Updating the policy :
+        optimizer.zero_grad()
+        loss = policy.calculateLoss(gamma)
+        loss.backward()
+        optimizer.step()        
+        policy.clearMemory()
+        
+        # saving the model if episodes > 999 OR avg reward > 200 
+        #if i_episode > 999:
+        #    torch.save(policy.state_dict(), './preTrained/LunarLander_{}_{}_{}.pth'.format(lr, betas[0], betas[1]))
+        
+        if running_reward > 4000 or i_episode == 1500:
+            torch.save(policy.state_dict(), './preTrained/LunarLander_{}_{}_{}.pth'.format(lr, betas[0], betas[1]))
+            print("########## Solved! ##########")
+            test(name='LunarLander_{}_{}_{}.pth'.format(lr, betas[0], betas[1]))
+            break
+        
+        if i_episode % 20 == 0:
+            running_reward = running_reward/20
+            print('Episode {}\tlength: {}\treward: {}'.format(i_episode, t, running_reward))
+            running_reward = 0
+            
+if __name__ == '__main__':
+    train()
